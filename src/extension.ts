377e017f92d5cb23e9b3147bbc09b3405bc5c4a9
Jocevičius, Karolis <karolis.jocevicius@seb.se>
2020-05-29 12:42:51 +0300

    sorting fix

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.languages.registerDocumentFormattingEditProvider('todo-lang', {
         provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
             let todoList = `\n$ Updated on ${new Date().toISOString()}`
-            const todoListLines = document.getText()
+            let todoListLines = document.getText()
                 .split(/\r?\n/)
                 .filter(v => v.trim().length > 2 && !v.startsWith('$'))
                 .map(l => {
@@ -16,10 +16,17 @@ export function activate(context: vscode.ExtensionContext) {
                     } else {
                         return l;
                     }
-                })
-                .sort((a, b) => b[1] > a[1] ? 1 : -1);
+                });
 
-            const doneCount = todoListLines.filter(l => l.startsWith('[*]')).length;
+            const doneLines = todoListLines
+                .filter(l => l.startsWith('[*]'))
+                .sort();
+            todoListLines = todoListLines
+                .filter(l => l.startsWith('[]'))
+                .sort();
+            todoListLines.push(...doneLines);
+
+            const doneCount = doneLines.length;
             todoList += `\n$ Done: ${doneCount}, Todo: ${todoListLines.length - doneCount}, Total: ${todoListLines.length}`;
             todoList += `\n$ ==================================================`
             todoList += '\n\n' + todoListLines.join('\n');
